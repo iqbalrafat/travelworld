5a8253befe57caadf764f565ebdd549becb57cd4
iqbalrafat <64336748+iqbalrafat@users.noreply.github.com>
2021-02-22 00:25:27 -0700

    [travelworld]--updated index file

--- index.js
+++ index.js
@@ -1,9 +1,21 @@
 const express = require('express');
 const cors=require('cors');
-
+const bodyParser = require('body-parser');
+const jwt = require ('jsonwebtoken');
+const dotenv = require('dotenv');
+const Joi = require('@hapi/joi');
+const bcrypt = require('bcryptjs');
+const cookieParser = require('cookie-parser');
 const app =express();
 
+//setup body and cookie parser
+app.use(express.json());
+app.use(bodyParser.json());
+// app.use(bodyParser({urlencoded:true}));
+app.use(cookieParser());
+
 app.get("/",(req,res)=>{
+  const data=req.body();
   res.send("hello world");
 })
 
