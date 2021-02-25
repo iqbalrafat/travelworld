dfe305de1966524b962f074349fe03e784e0245a
iqbalrafat <64336748+iqbalrafat@users.noreply.github.com>
2021-02-24 23:29:49 -0700

    [travelworld]--added route for Registration.

--- routes/authentication.js
+++ routes/authentication.js
@@ -20,10 +20,30 @@ router.post('/registration', (req,res)=>{
         CustUserName:Joi.string().min(6).require(),
         CustPassword:Joi.string().min(8).require(),
       });
+  const result= registerSchema.validate(body);
+  if(result.error)
+  return res.status(400).send(result.error.details[0].message);
+  const userExist=`SELECT * FROM customers where CustUserName="${body.CustUserName}" or CustEmail="${body.CustEmail}"`;
+  const registerQuery="INSERT INTO `Customers` (`CustFirstName`, `CustLastName`,`CustAddress`,`CustCity`,`CustProv`,`CustPostal`,`CustCountry`,`CustPhone`,"+
+  "`CustEmail`,`CustUserName`,`CustPassword`) VALUES(?,?,?,?,?,?,?,?,?,?,?,?)";
 
-
-
-
-
+  db.query(userExist,async(err,result)=>{
+      if(err) throw err;
+      if(result.length>0){
+      return res.status(400).send("user already exists");
+      }
+      const salt=await bcrypt.genSalt(10);
+      const encryptedPassword=await bcrypt.hash(body.CustPassword,salt);
+      body.CustPassword=encryptedPassword;
+    //inserting data into Database
+      let data=[]
+      for (let item of Object.keys(body)){
+        data.push(body[item])
+      }
+      db.query(registerQuery,data,(err,result)=>{
+        if(err) throw err;
+        return res.status(200).send(result);
+      });
+    });
 });
 module.exports=router;
