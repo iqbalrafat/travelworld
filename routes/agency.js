6123233f19406da229534fabc491ad12b13fc6fa
iqbalrafat <64336748+iqbalrafat@users.noreply.github.com>
2021-02-26 11:52:41 -0700

    [travelworld]--created Get API for all agents

--- routes/agency.js
+++ routes/agency.js
@@ -7,5 +7,14 @@ router.get('/agencyData',(req,res)=>{
   db.query(agencyDataQuery,(error,result)=>{
     if(error)throw error;
     return res.status(200).send(result);
-  })
-})
+  });
+});
+//Retrieve all agents from database
+router.get('/agentsData',(req,res)=>{
+  const agentsQuery=`SELECT * FROM agents`;
+  db.query(agentsQuery,(error,result)=>{
+    if(error)throw error;
+    res.status(200).send(result);
+  });
+});
+
